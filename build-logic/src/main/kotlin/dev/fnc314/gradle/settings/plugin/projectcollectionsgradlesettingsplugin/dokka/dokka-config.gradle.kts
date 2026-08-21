a19aeddd8cac436cd2580c98b9d2477ce68b8710
Franco N. Colaizzi <fnc314@gmail.com>
2026-08-21 00:59:52 -0400

    4.0.3

--- build-logic/src/main/kotlin/dev/fnc314/gradle/settings/plugin/projectcollectionsgradlesettingsplugin/dokka/dokka-config.gradle.kts
+++ build-logic/src/main/kotlin/dev/fnc314/gradle/settings/plugin/projectcollectionsgradlesettingsplugin/dokka/dokka-config.gradle.kts
@@ -51,7 +51,7 @@ dokka {
 
       sourceLink {
         localDirectory = layout.projectDirectory.dir("src")
-        remoteUrl = uri("https://github.com/fnc314/${rootProject.name}/${project.name}/tree/main/src")
+        remoteUrl = uri("https://github.com/fnc314/${project.name}/tree/main/src")
         remoteLineSuffix = "#L"
       }
 
