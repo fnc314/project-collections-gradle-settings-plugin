17a695c5c5f6845bc7218eec55000660c3df0004
Franco N. Colaizzi <fnc314@gmail.com>
2026-08-27 20:15:19 -0400

    5.0.3; source link fixes

--- sample/settings.gradle.kts
+++ sample/settings.gradle.kts
@@ -9,13 +9,13 @@ pluginManagement {
   includeBuild("../")
 
   plugins {
-    id("dev.fnc314.gradle.plugins.settings.project-collections-gradle-settings-plugin") version ("5.0.0")
+    id("dev.fnc314.gradle.plugins.settings.project-collections-gradle-settings-plugin") version ("5.0.3")
   }
 
 }
 
 plugins {
-  id("dev.fnc314.gradle.plugins.settings.project-collections-gradle-settings-plugin") version ("5.0.0")
+  id("dev.fnc314.gradle.plugins.settings.project-collections-gradle-settings-plugin") version ("5.0.3")
 }
 
 projectCollections {
